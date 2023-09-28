5c410f99bf7249c5e9c5fccec7f83c5fdb2f5b96
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-09-28 13:31:43 +0300

    improved typography

--- src/components/buttons/BigButton.jsx
+++ src/components/buttons/BigButton.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
 export default function BigButton({ num, specialKey = "reset-key" }) {
-  return <button className={`big-btn ${specialKey}`}>{num}</button>;
+  return (
+    <button className={`big-btn ${specialKey}`}>
+      <div className="vertical-align">{num}</div>
+    </button>
+  );
 }
 BigButton.propTypes = {
   num: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
